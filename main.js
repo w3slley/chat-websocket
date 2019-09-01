00d1b3e3ef4841341ffa83384a43a61a17ddb2d1
Weslley Victor <wvictor07@gmail.com>
2019-09-01 17:12:23 +0100

    Display users online functionality was added
    Created an array on the server containing all the usernames of users who are logged in. When a user is disconnected, its name is deleted from the array.

--- main.js
+++ main.js
@@ -4,23 +4,36 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 
+function deleteUser(users, deleteUser){//function to delete user from usersOnline array
+  for (let i=0;i<users.length;i++){
+    if(users[i] == deleteUser){
+        users.splice(i,1)
+    }
+  }
+  return users
+}
+
 app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
   res.sendFile(__dirname+'/index.html')
 })
-
+let numUsers = 0//initially num of users online is 0 because no one is online
+let usersOnline = []
 io.on('connection', function(socket){
   //console.log('a user connected');
   /*socket.on('disconnect', function(){
     console.log('a user disconnected');
   })*/
-
-  socket.on('username', function(username){
+  numUsers++ //it increases by one after a user logs in
+  socket.on('set username', function(username){
+    usersOnline.push(username)
+    console.log(usersOnline)
     socket.username = username
-    io.emit('joined', socket.username+' joined the room')
-    console.log(socket.username)
-
+    socket.broadcast.emit('joined', socket.username+' joined the room')
+  })
+  socket.on('get username', function(username){
+    io.emit('get username', usersOnline)
   })
   socket.on('chat message', function(msg){
     let data = {message: msg, username: socket.username, time: new Date() }
@@ -34,7 +47,15 @@ io.on('connection', function(socket){
   })
 
   socket.on('disconnect',function(){
-    io.emit('left', `${socket.username} left the room`)
+    io.emit('left',{message: `${socket.username} left the room`, username: socket.username})
+    deleteUser(usersOnline, socket.username)//deletes user from usersOnline array
+    console.log(usersOnline)
+    io.emit('delete username')
+    numUsers--
+    io.emit('number of users', numUsers)//passing num of users after one disconnects
+  })
+  socket.on('number of users', function(){
+    io.emit('number of users', numUsers)//passing num of users to view
   })
 
 })
--- public/app.js
+++ public/app.js
@@ -1,17 +1,20 @@
 const container = document.querySelector('.container')
 const socket = io()
 
+
 $('.setUsername').submit((e)=>{//sending username to server (will be stored in a session)
   e.preventDefault
-  socket.emit('username', $('.username').val())
+  socket.emit('set username', $('.username').val())
   $('.main').css('display','none')
   $('.container').css('display','block')
   $('.message').css('display', 'block')
   $('.message').focus()
+  socket.emit('number of users')
+  socket.emit('get username')
   return false
 
 })
-$('.form').submit(function(e){//sending message to server
+$('.msg').submit(function(e){//sending message to server
   e.preventDefault
   socket.emit('chat message', $('.message').val())
   $('.message').val('')
@@ -26,13 +29,21 @@ $('.message').keyup(function(){
 })
 
 
+socket.on('get username', function(usernames){
+    tag = ''
+    for(username of usernames){tag += '<li class='+username+'>'+username+'</li>'}
+    $('.users-online').html(tag)
+})
 socket.on('chat message', function(msg){
   container.scrollTop = container.scrollHeight-container.clientHeight//make div scroll to bottom
+  //container.scrollTo(0, container.scrollHeight+100) //this works too
+  console.log(container.scrollTop, container.clientHeight)
   $('.messages').append($('<li>').text(`${msg.username}:   ${msg.message}`))
 })
 socket.on('left', function(msg){
-  $('.messages').append($('<li class="status">').text(msg))
-  container.scrollTop = container.scrollHeight-container.clientHeight//make div scroll to bottom
+  $('.messages').append($('<li class="status">').text(msg.message))
+  $('.'+msg.username+'').remove()//removes the li with the username class. That is the way I found to easily delete the username li from the online users. I don't know if it's the best way though.
+  container.scrollTop = container.scrollHeight//make div scroll to bottom
 })
 
 socket.on('joined', function(msg){
@@ -44,4 +55,7 @@ socket.on('typing-in', function(username){
 })
 socket.on('typing-out', function(){
   $('.typing').text('')
+})
+socket.on('number of users', function(data){
+  $('.numUsers').text('('+data+')')
 })
